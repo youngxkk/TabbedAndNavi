d37b2ebd90a103acc5a205c57ebbe8f027ee88e1
仰星空 <sea@dashayudemacbookpro-15.local>
2018-06-29 14:08:18 +0800

    Initial Commit

---
+++ Tabbedapp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tabbedapp
+//
+//  Created by 大鲨鱼 on 2018/6/29.
+//  Copyright © 2018年 大鲨鱼. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
